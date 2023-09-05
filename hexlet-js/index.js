7775a18a26b44509047ec14a0206e28e483da964
Artem Yumatov <aayumatov@gmail.com>
2023-09-05 10:23:16 +0300

    fix files with eslint

--- hexlet-js/index.js
+++ hexlet-js/index.js
@@ -1,5 +1,6 @@
-import _ from "lodash";
-console.log("Hello, Hexlet!");
-_.capitalize("hello");
-_.intersection([2,1],[2,3]);
+import _ from 'lodash';
+
+console.log('Hello, Hexlet!');
+_.capitalize('hello');
+_.intersection([2, 1], [2, 3]);
 console.log(_.last(['one', 'two']));
